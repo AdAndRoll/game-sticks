f431c7ba5daa43ff229fb312c543ea78b2d404c3
AdAndRoll <vanovano03@mail.ru>
2024-11-11 16:33:50 +0300

    game sticks

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // Создаем экземпляр игры и запускаем графический интерфейс
+        DotsAndBoxesGame game = new DotsAndBoxesGame();
+        game.setVisible(true); // Отображаем окно игры
+    }
+}
